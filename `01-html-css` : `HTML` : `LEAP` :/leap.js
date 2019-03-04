ab204b212cbe057074ed95a48241adf46232cd83
dennisliukeikit <dennisliukeikit@gmail.com>
2019-03-04 22:19:59 +0800

    upload Exercism

--- `01-html-css` : `HTML` : `LEAP` :/leap.js
+++ `01-html-css` : `HTML` : `LEAP` :/leap.js
@@ -1,7 +1,46 @@
-export const isLeap = (num) => {
-    if (num % 4 === 0) {
+export const isLeap = (x) => {
+    if ((x%4 === 0) && (x%100 !== 0)) {
+        return true;
+    } else if ((x%100 === 0) && (x%400 === 0)) {
         return true;
     } else {
         return false;
+    }
+}
+
+
+/* Method 2
+export const isLeap = (x) => {
+    if (((x%4 === 0) && (x%100 !== 0)) || (x%400 === 0)) {
+        return true;
+    } else if ((x%100 === 0) && (x%400 !== 0)) {
+        return false;
+    } else {
+        return false;
+    }
+}
+*/
+
+
+/* Method 3
+export const isLeap = (year) => {
+    if (year % 4 !== 0) {
+        return false;
+    } else {
+        if (year % 100 !== 0) {
+            return true;
+        } else {
+            if (year % 400 !== 0) {
+                return false;
+                if (year % 200 === 0) {
+                    return false;
+                } else {
+                    return true;
+                }
+            } else {
+                return true;
+            }
+        };
     };
-};
+}
+*/
